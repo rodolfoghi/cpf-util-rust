2ac3212e3d9f64800b699aeaae7f3cd34161d7cb
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-04-02 00:27:06 -0300

    Remove RESERVED_NUMBERS

--- src/lib.rs
+++ src/lib.rs
@@ -17,20 +17,6 @@ pub fn reserved_numbers() -> Vec<String> {
     ]
 }
 
-
-pub const RESERVED_NUMBERS: &'static [&'static str] = &[
-    "00000000000",
-    "11111111111",
-    "22222222222",
-    "33333333333",
-    "44444444444",
-    "55555555555",
-    "66666666666",
-    "77777777777",
-    "88888888888",
-    "99999999999",
-];
-
 pub fn format(cpf: &str) -> String {
     let cpf = cpf.matches(char::is_numeric).collect::<Vec<_>>().concat();
 
