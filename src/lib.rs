382f2f9f81ba1297098934a929750ce99608d07c
Rodolfo Ghiggi <rodolfoghi@gmail.com>
2020-04-01 22:21:43 -0300

    Format document using vscode

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@ use std::cmp;
 
 const CPF_LENGTH: usize = 11;
 
-pub const RESERVED_NUMBERS: &'static [&'static str] = &[ "00000000000",
+pub const RESERVED_NUMBERS: &'static [&'static str] = &[
+    "00000000000",
     "11111111111",
     "22222222222",
     "33333333333",
@@ -12,26 +13,25 @@ pub const RESERVED_NUMBERS: &'static [&'static str] = &[ "00000000000",
     "77777777777",
     "88888888888",
     "99999999999",
-    ];
+];
 
-pub fn format(cpf: &str) -> String { 
-
-    let cpf = cpf.matches(char::is_numeric)
-        .collect::<Vec<_>>()
-        .concat();
+pub fn format(cpf: &str) -> String {
+    let cpf = cpf.matches(char::is_numeric).collect::<Vec<_>>().concat();
 
     if cpf.len() > 3 && cpf.len() <= 6 {
         format!("{}.{}", &cpf[0..3], &cpf[3..cpf.len()])
-    }
-    else if cpf.len() >= 7 && cpf.len() <= 9 {
+    } else if cpf.len() >= 7 && cpf.len() <= 9 {
         format!("{}.{}.{}", &cpf[0..3], &cpf[3..6], &cpf[6..cpf.len()])
-    }
-    else if cpf.len() >= 10 {
+    } else if cpf.len() >= 10 {
         let end_position = cmp::min(cpf.len(), CPF_LENGTH);
-        format!("{}.{}.{}-{}", &cpf[0..3], &cpf[3..6], &cpf[6..9], &cpf[9..end_position])
-    }
-    else 
-    {
+        format!(
+            "{}.{}.{}-{}",
+            &cpf[0..3],
+            &cpf[3..6],
+            &cpf[6..9],
+            &cpf[9..end_position]
+        )
+    } else {
         String::from(cpf)
     }
 }
